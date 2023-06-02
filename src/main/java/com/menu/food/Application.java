4804c475b6bf49de1b6a3f5aaa6203cd90a3e78b
Wendel Reis <wendel.m.w18@gmail.com>
2023-06-02 15:54:21 -0300

    fisrt-commit

---
+++ src/main/java/com/menu/food/Application.java
@@ -0,0 +1,13 @@
+package com.menu.food;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
