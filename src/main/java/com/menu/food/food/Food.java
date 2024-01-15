0880578bd7a4e5a4ecc92bf832c5bdbe27d00c73
Wendel S Reis <90618369+WendelSReis@users.noreply.github.com>
2024-01-14 21:54:12 -0300

    Update Food.java

--- src/main/java/com/menu/food/food/Food.java
+++ src/main/java/com/menu/food/food/Food.java
@@ -13,15 +13,19 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 public class Food {
+    
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    
     private Long id;
     private String title;
     private String image;
     private Integer price;
 
     public Food(FoodRequestDTO data){
+        
         this.image = data.image();
         this.price = data.price();
         this.title = data.title();
+        
     }
 }
